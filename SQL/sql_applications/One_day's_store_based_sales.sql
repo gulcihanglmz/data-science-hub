d1ce60b8f050e7fd20eb396cab1176911dd534a8
Gulcihan <gulmezgulcihan44@gmail.com>
2024-05-03 01:22:29 +0300

    Initial commit

---
+++ SQL/sql_applications/One_day's_store_based_sales.sql
@@ -0,0 +1,9 @@
+﻿SELECT DATE2,CITY,SUM(TOTALPRICE) AS TOTALPRICE
+FROM SALES 
+WHERE DATE2='2019-01-01'
+GROUP BY DATE2,CITY
+ORDER BY DATE2,SUM(TOTALPRICE) DESC
+
+--aynı DATE2 ve CITY değerlerine sahip satırlar bir araya getirilir 
+--ve bu grupların toplam fiyatları SUM(TOTALPRICE) ile hesaplanır.
+
